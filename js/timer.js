cf0cfc01535ecc31e5a8ff07554d21ae3469230a
iamyuna01 <iamu.design01@gmail.com>
2025-03-13 21:44:19 +0900

    [fix]시간 표시 디자인 변경

--- js/timer.js
+++ js/timer.js
@@ -3,16 +3,24 @@ let timer;
 
 function startTimer(updateCallback, endCallback){
     time = 120;
-    updateCallback(time);
+    updateCallback(formatTime(time));
 
     timer = setInterval(() => {
         time--;
-        updateCallback(time);
+        updateCallback(formatTime(time));
         if(time <= 0){
             clearInterval(timer);
             endCallback();
         }
     }, 1000);
+}
+
+function formatTime(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${String(minutes).padStart(2,'0')}:${String(remainingSeconds).padStart(2, '0')}`;
+
 }
 
 function stopTimer(){
